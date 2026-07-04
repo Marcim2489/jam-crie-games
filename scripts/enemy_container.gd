890d6597c15e8cc67ca1f069c66737e5f5305ab9
Marcim2489 <marciohgimenes@hotmail.com.br>
2026-07-04 18:29:48 -0300

    arrumando bug raro

--- scripts/enemy_container.gd
+++ scripts/enemy_container.gd
@@ -2,18 +2,20 @@ extends Node2D
 class_name EnemyContainer
 
 signal todosDerrotados
+var aDerrotar: int = 0
+var derrotados : int = 0
 
 func _ready() -> void:
 	y_sort_enabled = true
 	for inimigo in get_children():
 		if inimigo is Entidade:
+			aDerrotar +=1
 			inimigo.morreu.connect(verificarInimigos)
 
 func verificarInimigos():
-	var inimigos : Array[Entidade]
-	for c in get_children():
-		if c is Entidade:
-			inimigos.append(c)
-	if inimigos.size()-1 <= 0:
+	#for c in get_children():
+		#if c is Entidade:
+	derrotados += 1
+	if derrotados >= aDerrotar:
 		todosDerrotados.emit()
 		call_deferred("queue_free")
--- scripts/minimap.gd
+++ scripts/minimap.gd
@@ -10,24 +10,49 @@ class_name Minimap
 @export var rareIcon3 : Node2D
 @export var initialIcon : Node2D
 const RARE_ICON = preload("uid://12tt7au33e7j")
+var salaBoss : Vector2i
+var salaInicio : Vector2i
+var salaRara1 : Vector2i
+var salaRara2 : Vector2i
+var salaRara3 : Vector2i
 
 func _ready() -> void:
 	SalaManager.entrouNaSala.connect(mostrarSala)
+	bossIcon.visible = false
+	initialIcon.visible = false
+	rareIcon1.visible = false
+	rareIcon2.visible = false
+	rareIcon3.visible = false
 
 func posicionarIconesRaridade(salas : Array[Vector2i]):
-	rareIcon1.position = salas[0] * 16
-	rareIcon2.position = salas[1] * 16
-	rareIcon3.position = salas[2] * 16
+	salaRara1 = salas[0]
+	rareIcon1.position = salaRara1 * 16
+	salaRara2 = salas[1]
+	rareIcon2.position = salaRara2 * 16
+	salaRara3 = salas[2]
+	rareIcon3.position = salaRara3 * 16
 
 func posicionarIconeBoss(coord : Vector2i):
-	bossIcon.position = coord * 16
+	salaBoss = coord
+	bossIcon.position = salaBoss * 16
 
 func posicionarIconeInicial(coord : Vector2i):
-	initialIcon.position = coord * 16
+	salaInicio = coord
+	initialIcon.position = salaInicio * 16
 
 func colocarSala(sala : Vector2i, tipoDeSala : Vector2i):
 	baseMap.set_cell(sala, 1, tipoDeSala)
 
 func mostrarSala(sala : Vector2i):
 	playerIcon.position = sala * 16
+	if sala == salaBoss:
+		bossIcon.visible = true
+	if sala == salaInicio:
+		initialIcon.visible = true
+	if sala == salaRara1:
+		rareIcon1.visible = true
+	if sala == salaRara2:
+		rareIcon2.visible = true
+	if sala == salaRara3:
+		rareIcon3.visible = true
 	visibleMap.set_cell(sala, 0, baseMap.get_cell_atlas_coords(sala))
